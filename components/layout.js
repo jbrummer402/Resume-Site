e1ecf422c11b8165409072fecea4483dc6731505
Jack Brummer <jbrummer@stevens.edu>
2020-10-29 19:16:07 -0400

    initial commit

---
+++ components/layout.js
@@ -0,0 +1,62 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, DropdownButton } from 'react-bootstrap';
+
+const name = 'Jack Brummer'
+export const siteTitle = 'Resume Site'
+
+export default function Layout({ children, home }) {
+    return (
+        <>
+        <div>
+        <h1 style={{textAlign:"center"}} className={utilStyles.heading2Xl}>{name}</h1>
+        
+        <div style={{lineHeight: "1.0px"}} className={styles.container}>
+            <ul>
+                <li><Link href="/"><a>Home</a></Link></li>
+                <li>
+                    <div className={styles.dropdown}>
+                        <button className={styles.dropbtn}>
+                            Projects
+                        </button>
+                        <div className={styles.dropdownContent}>
+                            <a href="#">Minesweeper</a>
+                        </div>
+                    </div>
+                </li>
+                <li><Link href="/posts/about"><a>About</a></Link></li>
+                <li><Link href="/posts/contact"><a>Contact</a></Link></li>
+                <li><Link href="/comment"><a>Submit a comment</a></Link></li>
+            </ul>
+            
+        <header className={styles.header}>
+            {home ? (
+            <>
+                <img
+                src="/images/Jack_Brummer (1).jpg"
+                className={`${styles.headerHomeImage}`}
+                alt={name}
+                />
+                <section style={{paddingLeft: "15px"}} className={utilStyles.headingMd}>
+                    <h1>Welcome to my website</h1>
+                    <p>
+                    Here you'll find some of my favorite projects that I have worked on over the years.
+                    I hope you enjoy it!
+                    </p>
+                </section>
+            </>
+            ) : (
+            <>
+                
+            </>
+            )}
+      </header>
+      <main>{children}</main>
+      
+    </div>
+    </div>
+    </>
+    )
+}
---
+++ posts/contact.md
@@ -0,0 +1,5 @@
+---
+title: 'Contact'
+---
+You can reach me at my email: jbrummer@stevens.edu <br/>
+Or my cell phone: 917-510-5799
