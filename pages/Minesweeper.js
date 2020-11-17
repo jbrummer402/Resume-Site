119fdff79d894f76aabe508a5fea4d1fc0b9d9f8
Jack Brummer <jbrummer@stevens.edu>
2020-11-17 11:03:24 -0500

    attempting to embed minesweeper code

---
+++ pages/Minesweeper.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/utils.module.css'
+import Layout, { siteTitle } from "../components/layout"
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+function applet() {
+  return {__html:
+            <applet code="AppletCode/APCS2017Minesweeper/silverlight/games/Minesweeper.class"></applet>
+          };
+}
+
+export default function Minesweeper() {
+    return (
+
+      <>
+      <Layout>
+          <div dangerouslySetInnerHTML={applet()}/>
+      </Layout>
+      </>
+    )
+
+}
