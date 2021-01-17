45c115e0b2a6c353ced396802f2fd05a73211899
Jack Brummer <jbrummer@stevens.edu>
2021-01-17 17:38:07 -0500

    imported grid from material ui

--- pages/Projects.js
+++ pages/Projects.js
@@ -3,18 +3,18 @@ import styles from '../components/layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import Layout from "../components/layout"
-
+import Grid from '@material-ui/core/Grid'
 
 export default function Projects() {
     return (
       <>
         <Layout>
-        <article>
-            <h1 className={utilStyles.heading2Xl}>Projects</h1>
+            <article>
+                <h1 className={utilStyles.heading2Xl}>Projects</h1>
+                <p style={{fontSize: "1.5rem",lineHeight: "1.6"}}> Here is an overview of the projects I have done! </p>
 
-            <div style={{fontSize: "1.5rem",lineHeight: "1.6"}}> Here is an overview of the projects I have done! </div>
+            </article>
 
-        </article>
         </Layout>
       </>
     )
