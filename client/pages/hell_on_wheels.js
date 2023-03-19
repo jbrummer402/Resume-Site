373712b21d9c6caab5782ebcc9f65ce21784697a
Jack Brummer <jbrummer@stevens.edu>
2023-03-19 17:09:14 -0400

    dependency conflicts fixed

--- client/pages/hell_on_wheels.js
+++ client/pages/hell_on_wheels.js
@@ -16,7 +16,17 @@ import {
   VStack,
 } from '@chakra-ui/react';
 
+import { axios } from 'axios'
 
+import { useState, useEffect } from 'react';
+import { isEmpty } from 'lodash';
+
+const blogURL = 'jackbrummer.com/wp-json/wp/v2/posts'
+
+async function loading() {
+  let [loading, setLoading] = useEffect(false);
+
+}
 
 const BlogTags = (props) => {
   return (
@@ -32,6 +42,9 @@ const BlogTags = (props) => {
   );
 };
 
+async function getPosts() {
+
+}
 
 export const BlogAuthor = (props) => {
   return (
@@ -39,7 +52,7 @@ export const BlogAuthor = (props) => {
       <Image
         borderRadius="full"
         boxSize="40px"
-        src="https://100k-faces.glitch.me/random-image"
+        src="/images/IMG_4380.png"
         alt={`Avatar of ${props.name}`}
       />
       <Text fontWeight="medium">{props.name}</Text>
@@ -49,129 +62,31 @@ export const BlogAuthor = (props) => {
   );
 };
 
-const ArticleList = () => {
-  return (
-    <Container maxW={'7xl'} p="12">
-      <Heading as="h1">Hell on Wheels</Heading>
-      <Box
-        marginTop={{ base: '1', sm: '5' }}
-        display="flex"
-        flexDirection={{ base: 'column', sm: 'row' }}
-        justifyContent="space-between">
-        <Box
-          display="flex"
-          flex="1"
-          marginRight="3"
-          position="relative"
-          alignItems="center">
-          <Box
-            width={{ base: '100%', sm: '85%' }}
-            zIndex="2"
-            marginLeft={{ base: '0', sm: '5%' }}
-            marginTop="5%">
-            <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-              <Image
-                borderRadius="lg"
-                src={
-                  'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                }
-                alt="some good alt text"
-                objectFit="contain"
-              />
-            </Link>
-          </Box>
-          <Box zIndex="1" width="100%" position="absolute" height="100%">
-            <Box
-              bgGradient={useColorModeValue(
-                'radial(orange.600 1px, transparent 1px)',
-                'radial(orange.300 1px, transparent 1px)'
-              )}
-              backgroundSize="20px 20px"
-              opacity="0.4"
-              height="100%"
-            />
-          </Box>
-        </Box>
-        <Box
-          display="flex"
-          flex="1"
-          flexDirection="column"
-          justifyContent="center"
-          marginTop={{ base: '3', sm: '0' }}>
-          <BlogTags tags={['Engineering', 'Product']} />
-          <Heading marginTop="1">
-            <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-              Blog article title
-            </Link>
-          </Heading>
-          <Text
-            as="p"
-            marginTop="2"
-            color={useColorModeValue('gray.700', 'gray.200')}
-            fontSize="lg">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </Text>
-          <BlogAuthor name="John Doe" date={new Date('2021-04-06T19:01:27Z')} />
-        </Box>
-      </Box>
-      <Heading as="h2" marginTop="5">
-        Recent Articles
-      </Heading>
-      <Divider marginTop="5" />
-      <Wrap spacing="30px" marginTop="5">
-        <WrapItem width={{ base: '100%', sm: '45%', md: '45%', lg: '30%' }}>
-          <Box w="100%">
-            <Box borderRadius="lg" overflow="hidden">
-              <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                <Image
-                  transform="scale(1.0)"
-                  src={
-                    'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                  }
-                  alt="some text"
-                  objectFit="contain"
-                  width="100%"
-                  transition="0.3s ease-in-out"
-                  _hover={{
-                    transform: 'scale(1.05)',
-                  }}
-                />
-              </Link>
-            </Box>
-            <BlogTags tags={['Engineering', 'Product']} marginTop="3" />
-            <Heading fontSize="xl" marginTop="2">
-              <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                Some blog title
-              </Link>
-            </Heading>
-            <Text as="p" fontSize="md" marginTop="2">
-              
-            </Text>
-            <BlogAuthor
-              name="John Doe"
-              date={new Date('2021-04-06T19:01:27Z')}
-            />
-          </Box>
-        </WrapItem>
-      </Wrap>
-      <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
-        <Heading as="h2">What I write about</Heading>
-        
-        <Text as="p" fontSize="lg">
-        
-        </Text>
-        <Text as="p" fontSize="lg">
-          
-        </Text>
-        <Text as="p" fontSize="lg">
+function ArticleList(props) {
 
-        </Text>
-      </VStack>
-    </Container>
-  );
+  let [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    let posts = getPosts()
+  })
+
+  if (isEmpty(posts)) {
+    return (
+      <Container maxW={'7xl'} p="12">
+        <Heading align='center' fontSize={'5xl'} as="h1">Hell on Wheels</Heading>
+        
+        <Heading align='left' as='h2' marginTop='5'>End of posts</Heading>
+        <Text align='left' fontSize='2xl'>Enter the mailing list to stay up to date!</Text> 
+        
+        <Divider marginTop="5" />
+        
+        <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
+          <Heading as="h2">What I write about</Heading>        
+        </VStack>
+      </Container>
+    );
+  }
+  
 };
 
 export default ArticleList;
