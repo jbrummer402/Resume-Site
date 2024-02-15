0a15aa9c58f5c1cc60cb6dd10625d5ca8eb976fb
Jack Brummer <jbrummer402@gmail.com>
2024-02-15 11:56:06 -0500

    removed api function in types

--- client/types/post.ts
+++ client/types/post.ts
@@ -5,13 +5,4 @@ export type Post = {
 	content: string;
 }
 
-function authenticate(params:type) {
-	let auth = false;
 
-	try {
-				
-	} catch (error) {
-		console.error(error);
-	}
-	return auth;
-}
