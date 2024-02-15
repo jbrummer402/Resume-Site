1c0882239a1184cbb8d8b4e767cb01aae1b538fc
Jack Brummer <jbrummer402@gmail.com>
2024-02-15 12:08:44 -0500

    change to https

--- client/lib/api/data.ts
+++ client/lib/api/data.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function getPosts() {
   try {
     const { data } = await axios.get(
-`http://${process.env.LOCAL_URL}/all_posts`);
+`https://${process.env.LOCAL_URL}/all_posts`);
 
     return data;
   } catch (error) {
@@ -14,7 +14,7 @@ export async function getPosts() {
 
 export async function getPostId(id) {
   const { data } = await axios.get(
-    `http://${process.env.LOCAL_URL}/posts/${id}`,
+    `https://${process.env.LOCAL_URL}/posts/${id}`,
   );
   return data;
 }
