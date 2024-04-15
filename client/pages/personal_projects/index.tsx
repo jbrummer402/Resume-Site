115a0d0abc3b1296e91337171fd46f6ee3398571
Jack Brummer <jbrummer@stevens.edu>
2024-04-15 11:14:50 -0400

    cors issue fixed

--- client/pages/personal_projects/index.tsx
+++ client/pages/personal_projects/index.tsx
@@ -1,30 +1,55 @@
-
 import axios from "axios";
 import { Heading, Container, Text } from "@chakra-ui/react";
-import ProjectBox from "../../components/containers/project_box"
+import ProjectBox from "../../components/containers/project_box";
+import { useEffect, useState } from "react";
 
-export default function PersonalProjects(params:type) {
+export default function PersonalProjects(params: type) {
+  let [repos, setRepos] = useState([]);
+
+  const getRepos = async () => {
+    let data = await fetch("http://127.0.0.1:8000/app/repos");
+    return data;
+  };
+  useEffect(() => {
+    (async () => {
+      try {
+        console.log("Fetching repos");
+        const response = await getRepos();
+        let data = await response.text(); // Extract JSON data from response
+
+        data = JSON.parse(data);
+        console.log(JSON.parse(data));
+        setRepos(JSON.parse(data));
+      } catch (err) {
+        console.log("Error occurred when fetching books");
+      }
+    })();
+  }, []);
+
   return (
-    <Container
-      maxW={"80%"}
-    >
+    <Container maxW={"80%"}>
       <Heading pt="10rem"> Personal Projects </Heading>
 
-      <Text fontSize='2xl'>
-        These projects include those from my time in college and also from personal study. 
+      <Text fontSize="2xl">
+        These projects include those from my time in college and also from
+        personal study.
       </Text>
-
-      <ProjectBox title={"Project"}/>
-
-    </Container>);
+      {repos.map((repo) => {
+        return (
+          <ProjectBox
+            key={repo.id}
+            title={repo.name}
+            description={repo.description}
+            link={repo.link}
+          />
+        );
+      })}
+    </Container>
+  );
 }
 
-export const getStaticProp = (async () => {
-
+export const getStaticProp = async () => {
   try {
     const data = await getRepos();
-  } catch (e) {
-    
-  }
-
-})
+  } catch (e) {}
+};
